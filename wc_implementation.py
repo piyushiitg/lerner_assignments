16f7f38b8850b446f08f0b5f018bd978d65acf2a
Piyush Singhai <piyush.singhai@scalearc.com>
2015-10-26 12:19:42 -0700

    Added lerner solution in it too

--- wc_implementation.py
+++ wc_implementation.py
@@ -41,7 +41,23 @@ def wc_implementation(file_name):
     print "Number of words (separated by whitespace): ", no_of_words
     print "Number of lines: ", number_of_lines
     print "number_of_unique_words", len(set(words))
- 
+
+def lerner_solution():
+    filename = raw_input("Enter a filename: ")
+     
+    number_of_characters = 0
+    number_of_words = 0
+    unique_words = set()
+      
+    for number_of_lines, line in enumerate(open(filename), 1):
+        number_of_characters += len(line)
+        number_of_words += len(line.split())
+        unique_words.update(line.split())
+               
+    print("Number of lines: {}".format(number_of_lines))
+    print("Number of characters: {}".format(number_of_characters))
+    print("Number of words: {}".format(number_of_words))
+    print("Number of unique words: {}".format(len(unique_words)))
 
 if __name__ == "__main__":
     import sys
